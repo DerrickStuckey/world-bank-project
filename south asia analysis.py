81a4cca6d5d5fba33cfbeb8c5b5f7e4b53f2ce02
John Derrick Stuckey <dstuckey@johns-air.home>
2014-12-07 21:48:44 -0500

    cleaned up

--- south asia analysis.py
+++ south asia analysis.py
@@ -11,50 +11,58 @@ import wbdata
 import pandas
 import matplotlib.pyplot as plt
 
- 
-#set up the countries I want
-countries = ['BGD','IND','LKA','MDV','NPL']
-#countries = ['BGD','BTN','IND','LKA','MDV','NPL']
- 
-#set up the indicator I want (just build up the dict if you want more than one)
-#indicators = {'1.1_TOTAL.FINAL.ENERGY.CONSUM':'Total Energy Consumption'} #good for south asia
-#indicators = {'NY.GDP.MKTP.CD':'GDP'} #good for south asia
-#indicators = {'4.1.1_TOTAL.ELECTRICITY.OUTPUT':'Electricity Output'} #good for south asia
-#indicators = {'SP.POP.TOTL':'Total Population'} #good for south asia
-#indicators = {'SP.URB.TOTL.IN.ZS':'% Urban Pop',} #good for south asia
+saveDir = "./data"
+createCSVs = True
 
+# Read DF from CSV
+def readFromCSV(dfName):
+    df = pd.read_csv(saveDir + '/' + dfName + '.csv')
+    return df
+
+# Function to save a dataframe to CSV
+def saveToCsv(df, dfName):
+    df.to_csv(saveDir + '/' + dfName + '.csv')
+
+# All countries in the "South Asia" region, 
+# except Afghanistan which is missing data
+countries = ['BGD','IND','LKA','MDV','NPL','BTN','PAK']
+
+# Names for each indicator to be used throughout
 elecIndName = 'Electricity Output'
 energyIndName = 'Total Energy Consumption'
+gdpIndName = 'GDP'
+popIndName = 'Total Population'
+
 indicators = {'4.1.1_TOTAL.ELECTRICITY.OUTPUT':elecIndName,
-              '1.1_TOTAL.FINAL.ENERGY.CONSUM':energyIndName}
+              '1.1_TOTAL.FINAL.ENERGY.CONSUM':energyIndName,
+              'NY.GDP.MKTP.CD':gdpIndName,
+              'SP.POP.TOTL':popIndName}
 
 #grab indicators above for countires above and load into data frame
 df = wbdata.get_dataframe(indicators, country=countries, convert_date=False)
 
-print "\ndf.head():\n", df.head()
+#debugging:
+#print "\ndf.head():\n", df.head()
 
 #df is "pivoted", pandas' unstack fucntion helps reshape it into something plottable
 dfu = df.unstack(level=0)
 
-#or get rid of multi-index
+#dataframe of just electricity values
 dfuElec = dfu[elecIndName]
-print "\nElectricity columns: \n", dfuElec.columns
 
-print "\nElec head and tail: \n", 
-print dfuElec.head()
-print dfuElec.tail()
-
-#or get rid of multi-index
+#dataframe of just Energy values
 dfuEnergy = dfu[energyIndName]
-print "\nEnergy columns: \n", dfuEnergy.columns
 
-print "\nEnergy head and tail: \n", 
-print dfuEnergy.head()
-print dfuEnergy.tail()
- 
-# a simple matplotlib plot with legend, labels and a title
-#dfu.plot(); 
-#plt.legend(loc='best'); 
-#plt.title("Energy Consumption"); 
-#plt.xlabel('Date'); 
-#plt.ylabel('Energy Consumption');
+#dataframe of just GDP values
+dfuGDP = dfu[gdpIndName]
+
+#dataframe of just Population values
+dfuPop = dfu[popIndName]
+
+# if flagged, create a set of CSV files, one for each indicator
+if (createCSVs):
+    saveToCsv(dfu, "South_Asian_Stats")
+    saveToCsv(dfuElec, "South_Asian_ELectricity_Production")
+    saveToCsv(dfuEnergy, "South_Asian_Energy_Usage")
+    saveToCsv(dfuGDP, "South_Asian_GDP")
+    saveToCsv(dfuPop, "South_Asian_Population")
