eac2182cf6066fa37df13c55ce6d918f7a595da1
John Derrick Stuckey <dstuckey@johns-air.home>
2014-12-07 22:30:43 -0500

    R code

---
+++ data_manipulation.R
@@ -0,0 +1,10 @@
+South_Asian_Stats <- read.csv("./South_Asian_Stats.csv")
+
+bangladeshStats <- South_Asian_Stats[South_Asian_Stats$country == "Bangladesh",]
+bhutanStats <- South_Asian_Stats[South_Asian_Stats$country == "Bhutan",]
+indiaStats <- South_Asian_Stats[South_Asian_Stats$country == "India",]
+sriLankaStats <- South_Asian_Stats[South_Asian_Stats$country == "SriLanka",]
+maldivesStats <- South_Asian_Stats[South_Asian_Stats$country == "Maldives",]
+nepalStats <- South_Asian_Stats[South_Asian_Stats$country == "Nepal",]
+pakistanStats <- South_Asian_Stats[South_Asian_Stats$country == "Pakistan",]
+
