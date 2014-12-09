fba5db48084571562e8fe832a78acf3002cc6eec
John Derrick Stuckey <dstuckey@Johns-MacBook-Air.local>
2014-12-09 17:09:25 -0500

    stable version

--- data_analysis.R
+++ data_analysis.R
@@ -6,5 +6,5 @@ rSquaredVals <- lapply(countryEnergyRegression, function(x) {summary(x)[[8]]})
 correlationVals <- sqrt(as.double(rSquaredVals))
 
 #create a bar chart of the correlation between GDP and Energy Consumption for each regression model
-barplot(as.double(correlationVals), ylab="correlation", xlab="country", col="blue", main="GDP, Energy Correlation", names=substr(names(countryStats),0,3))
+barplot(as.double(correlationVals), ylim=c(0.0,1.0), ylab="correlation", xlab="country", col="blue", main="Correlation: GDP vs. Energy Consumption", names=substr(names(countryStats),0,3))
 
