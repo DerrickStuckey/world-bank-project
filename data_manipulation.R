8dbae2dec71e2582944f94cfa1b064900e8120d2
John Derrick Stuckey <dstuckey@johns-air.home>
2014-12-09 12:51:16 -0500

    refactored into multiple files

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,9 +1,13 @@
+# Read in full dataset
 South_Asian_Stats <- read.csv("./South_Asian_Stats.csv")
+
+# Calculate derived attributes
 South_Asian_Stats$EnergyIntensity <- South_Asian_Stats$GDP / South_Asian_Stats$Total.Energy.Consumption
 #South_Asian_Stats$ElectricityIntensity <- South_Asian_Stats$GDP / South_Asian_Stats$Electricity.Output
 South_Asian_Stats$EnergyPerCapita <- South_Asian_Stats$Total.Energy.Consumption / South_Asian_Stats$Total.Population
 South_Asian_Stats$GDPPerCapita <- South_Asian_Stats$GDP / South_Asian_Stats$Total.Population
 
+# Construct a separate dataframe for each country to be analyzed
 bangladeshStats <- South_Asian_Stats[South_Asian_Stats$country == "Bangladesh",]
 bhutanStats <- South_Asian_Stats[South_Asian_Stats$country == "Bhutan",]
 indiaStats <- South_Asian_Stats[South_Asian_Stats$country == "India",]
@@ -12,22 +16,9 @@ maldivesStats <- South_Asian_Stats[South_Asian_Stats$country == "Maldives",]
 nepalStats <- South_Asian_Stats[South_Asian_Stats$country == "Nepal",]
 pakistanStats <- South_Asian_Stats[South_Asian_Stats$country == "Pakistan",]
 
-library(ggplot2)
-ggplot(data = South_Asian_Stats, aes(x=date, y=EnergyIntensity)) + ggtitle("South Asia Energy Intensity of GDP") + xlab("Year") + ylab("Energy Intensity of GDP") + geom_line(aes(colour=country))
-No_Maldives <- South_Asian_Stats[South_Asian_Stats$country != "Maldives",]
-ggplot(data = No_Maldives, aes(x=date, y=EnergyIntensity)) + ggtitle("Selected Countries Energy Intensity of GDP") + xlab("Year") + ylab("Energy Intensity of GDP") + geom_line(aes(colour=country))
-
+# Store the country-specific dataframes in a list for batch processing
 countryStats <- list(bangladeshStats, bhutanStats, indiaStats, sriLankaStats, maldivesStats, nepalStats, pakistanStats)
 names(countryStats) <- c("Bangladesh", "Bhutan", "India", "Sri Lanka", "Maldives", "Nepal", "Pakistan")
 
-#run a regression of GDP vs Energy Consumption on each country stat set
-runRegression <- function(x) {lm(GDP ~ Total.Energy.Consumption, data=x)}
-countryEnergyRegression <- lapply(countryStats, runRegression)
-beta1Coeffs <- lapply(countryEnergyRegression, function(x) {x$coefficients[[2]]})
-rSquaredVals <- lapply(countryEnergyRegression, function(x) {summary(x)[[8]]})
-correlationVals <- sqrt(as.double(rSquaredVals))
-  
-#create a bar chart of the beta1 coefficients for each regression model
-barplot(as.double(correlationVals), col="blue", main="GDP, Energy Correlation", names=substr(names(countryStats),0,3))
 
 
