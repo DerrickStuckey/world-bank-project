71b77fcafd57cd9f45c0ac7ac243ad53a5732639
John Derrick Stuckey <dstuckey@johns-air.home>
2014-12-09 23:28:35 -0500

    added mysql interaction

--- south asia analysis.py
+++ south asia analysis.py
@@ -8,11 +8,22 @@ GWU Programming for Analytics Individual Project
 """
 
 import wbdata
-import pandas
+import pandas as pd
 import datetime
+import MySQLdb as myDB
+from sqlalchemy import create_engine
 
+#Run parameters
 saveDir = "./data"
 createCSVs = False
+useDB = False
+
+#Database configuration info
+mysql_host = "173.194.241.40"
+mysql_user = "root"
+mysql_pass = "beer"
+mysql_db = "worldbank"
+data_table = "south_asia_stats"
 
 # Read DF from CSV
 def readFromCSV(dfName):
@@ -22,6 +33,29 @@ def readFromCSV(dfName):
 # Function to save a dataframe to CSV
 def saveToCsv(df, dfName):
     df.to_csv(saveDir + '/' + dfName + '.csv')
+    
+# Returns single DB Connect object to be used multiple times
+def getDBConnect():
+    return myDB.connect(host=mysql_host,
+                        user=mysql_user,
+                        passwd=mysql_pass,
+                        db=mysql_db)
+       
+# Function to save DataFrame to MySQL database
+def saveToDB(df, table, dbConnect):
+    #clean up any 'NaN' fields which mysql doesn't understand
+    dfClean = df.where((pd.notnull(df)), None)
+    dfClean.to_sql(con=dbConnect,
+                    name=table,
+                    if_exists="replace",
+                    flavor='mysql')
+
+# Missing sqlalchemy.schema module
+def readFromDB(table, dbConnect):
+    engine = create_engine('mysql+mysqldb://' + mysql_user + ':' + mysql_pass + '@' + mysql_host + '/' + mysql_db)
+    
+    df = pd.read_sql_table(table, con=engine)
+    return df
 
 # All countries in the "South Asia" region, 
 # except Afghanistan which is missing data
@@ -45,10 +79,13 @@ endTime = datetime.datetime(year=2009,month=12,day=31)
 df = wbdata.get_dataframe(indicators, country=countries, convert_date=False,
                           data_date=(startTime,endTime))
 
-#debugging:
-#print "\ndf.head():\n", df.head()
+#If useDB flag is True, save the dataframe to the database and read it back out
+if (useDB):
+    dbCon = getDBConnect()
+    saveToDB(df, data_table, dbCon)
+    df = readFromDB(data_table, dbCon)
 
-#df is "pivoted", pandas' unstack fucntion helps reshape it into something plottable
+#unstack dataframe to separate into one dataframe per indicator
 dfu = df.unstack(level=0)
 
 #dataframe of just electricity values
